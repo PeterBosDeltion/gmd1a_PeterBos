b5ac90ac3274b5fc55c6dab6dd5d5cb0c759d37d
gmd1apeterbos <97040824@st.deltion.nl>
2016-10-05 13:51:25 +0200

    Unity project
    Het pinball project

---
+++ pinball project/Assets/Scripts/Fliprechts.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fliprechts : MonoBehaviour
+{
+    public Rigidbody rb2;
+    // Use this for initialization
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            rb2.AddForce(transform.forward * 500);
+            transform.Rotate(transform.rotation.x +0, transform.rotation.y +0, transform.rotation.z + 90);
+        }
+        else if (Input.GetKeyUp("right"))
+        {
+            rb2.AddForce(transform.forward * -500);
+            transform.Rotate(transform.rotation.x -0, transform.rotation.y -0, transform.rotation.z - 90);
+        }
+
+    }
+}
---
+++ pinball project/Assets/Scripts/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Launch();
+	}
+    public void Launch()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(transform.forward *-4000); 
+        } else if (Input.GetKeyUp("space"))
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.position = new Vector3(3.98F, 0.56F, -17.28F);
+        }
+    }
+}
