26ceac7701040c425de387178593be315ec8352d
gmd1apeterbos <97040824@st.deltion.nl>
2017-04-09 20:39:13 +0200

    Project 3
    Vluchteling

---
+++ P3/Project Vluchteling/Project vluchteling/Assets/Scripts/Conversmanager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Conversmanager : MonoBehaviour {
+    public static bool inConv;
+    public InputField talkConv;
+    public List<string> insults = new List<string>();
+    public string insultConv;
+    public static string npcConv;
+    public Text npcScrnTxt;
+    public static int posInt;
+    public static int negInt;
+    public static Npc npc;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        talkConv.onEndEdit.AddListener(delegate { Talking(talkConv); });
+        negInt = 0;
+        posInt = 0;
+        
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void Talking(InputField input)
+    {
+
+        if (input.text == "A" || input.text == "a")
+        {
+            postCont();
+        }
+        else if (input.text == "B" || input.text == "b")
+        {
+            negtCont();
+        }
+        else
+        {
+            calcInsult();
+        }
+    }
+
+    public void calcInsult()
+    {
+        int i = Random.Range(0, insults.Count);
+        npcConv = insults[i];
+        gameObject.GetComponent<Uimanager>().setText();
+
+    }
+
+    public void clearTxt()
+    {
+        npcConv = "";
+        npcScrnTxt.text = "";
+    }
+
+    public void postCont()
+    {
+        posInt += 1;
+        npc.talkPos();
+       
+        
+    }
+
+    public void negtCont()
+    {
+        negInt += 1;
+        posInt = 0;
+        npc.talkNeg();
+    }
+
+    public void clearCont()
+    {
+        negInt = 0;
+        posInt = 0;
+    }
+
+    public void sendTxt()
+    {
+        gameObject.GetComponent<Uimanager>().setText();
+    }
+}
---
+++ P3/Project Vluchteling/Project vluchteling/Assets/Scripts/Enemy.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public List<GameObject> wp = new List<GameObject>();
+    public float speed;
+    public Transform target;
+    public Transform player;
+    public GameObject speler;
+    public bool inRange;
+    public int index;
+    public int stopRange;
+    public GameObject flash;
+    public RaycastHit hit;
+    public RaycastHit hut;
+    public Ray ray;
+    public bool obstructed;
+    public float detectRate;
+    public Rigidbody rb;
+    public float stuckTimer;
+    public static bool isHard;
+
+
+
+    // Use this for initialization
+    void Start() {
+        index = 0;
+        speed = 5;
+        detectRate = 0.25F;
+        flash.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        detectWalls();
+        antiStuck();
+        notAttacking();
+        makeHarder();
+
+
+    }
+
+    void FixedUpdate()
+    {
+        patrol();
+        movetoPlayer();
+
+
+
+    }
+
+    public void movetoPlayer()
+    {
+        if (inRange == true)
+        {
+            if (Vector3.Distance(transform.position, player.position) > stopRange)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+                Vector3 targetDir = player.position - transform.position;
+                Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, speed, 0.0F);
+                transform.rotation = Quaternion.LookRotation(lookDir);
+                
+            }
+
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = true;
+            Attack();
+        }
+       
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+
+    public void patrol()
+    {
+        if (inRange == false) {
+            transform.position = Vector3.MoveTowards(transform.position, wp[index].transform.position, speed * Time.deltaTime);
+            target = wp[index].transform;
+            Vector3 targetDir = target.position - transform.position;
+            Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, speed, 0.0F);
+            transform.rotation = Quaternion.LookRotation(lookDir);
+            if (index < wp.Count)
+            {
+                if (transform.position == wp[index].transform.position)
+                {
+                    index++;
+                }
+            }
+            if (index == wp.Count)
+            {
+                index = 0;
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        if (obstructed == false)
+        {
+            if (inRange == true)
+            {
+
+                flash.SetActive(true);
+                Player.detection += detectRate;
+                
+            }
+   
+        }
+
+
+    }
+
+    public void notAttacking()
+    {
+        if (inRange == false)
+        {
+            flash.SetActive(false);
+            
+            
+                if(flash.activeSelf == false)
+                {
+                    if (Player.detection > 0)
+                    {
+                        Player.detection -= 0.5F;
+
+                    }
+               
+                
+            }
+            
+        }
+        
+    }
+
+    public void detectWalls()
+    {
+
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
+        {
+            if (hit.transform.tag != "Player")
+            {
+                obstructed = true;
+            }
+            else if (hit.transform.tag == "Player")
+            {
+                obstructed = false;
+            }
+        }
+    }
+
+    public void antiStuck()
+    {
+        if (obstructed == true)
+        {
+            stuckTimer += Time.deltaTime;
+            if (stuckTimer >= 20)
+            {
+                transform.position = wp[0].transform.position;
+                stuckTimer = 0;
+                obstructed = false;
+                index = 0;
+            }
+        }
+        
+    }
+
+    public void makeHarder() //( ͡° ͜ʖ ͡°)
+    {
+        if(isHard == true)
+        { 
+        speed = 6;
+        detectRate = 1;
+        }
+    }
+}
+
+
+   
+
+
+
+
+
